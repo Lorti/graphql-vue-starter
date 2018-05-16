a7d4b0cd4048a5f0eba9975f563689d6cc500f1b
Manuel Wieser <office@manuelwieser.com>
2018-05-16 12:12:16 +0200

    Change indentation from 4 to 2 spaces

--- client/src/main.js
+++ client/src/main.js
@@ -8,31 +8,31 @@ import VueApollo from 'vue-apollo'
 
 // An Apollo Link to allow sending a single http request per operation.
 const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql'
+  uri: 'http://localhost:4000/graphql'
 })
 
 // Apollo Client is a fully-featured caching GraphQL client,
 // which has view layer integrations for all the popular front-end frameworks.
 const apolloClient = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-    connectToDevTools: true
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
 })
 
 // This plugin integrates Apollo Client in your Vue.js components with declarative queries.
 Vue.use(VueApollo)
 
 const apolloProvider = new VueApollo({
-    defaultClient: apolloClient
+  defaultClient: apolloClient
 })
 
 new Vue({
-    el: '#app',
+  el: '#app',
 
-    // Like vue-router or vuex, you need to specify the apolloProvider object on your root components.
-    // A provider holds the apollo client instances that can then be used by all the child components.
-    apolloProvider,
+  // Like vue-router or vuex, you need to specify the apolloProvider object on your root components.
+  // A provider holds the apollo client instances that can then be used by all the child components.
+  apolloProvider,
 
-    // The `h` stands for `Hyperscript`, and is an alias to `createElement`.
-    render: h => h(App)
+  // The `h` stands for `Hyperscript`, and is an alias to `createElement`.
+  render: h => h(App)
 })
